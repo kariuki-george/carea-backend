b44a585520cc6949d7c39402775fb791ef6f0719
smiley-geek <kariukigeorge2030@gmail.com>
2022-07-21 10:01:03 +0000

    fix: listen to env PORT in prod

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
-  await app.listen(4000);
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
